a9d997452a09128068d4224870d1d7e59b1292f6
falcon <9504402@qq.com>
2020-12-14 10:53:27 +0800

    新建样例项目

---
+++ src/Faclon.StoredProcedureRunner.Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Faclon.StoredProcedureRunner.Example.Database;
+using Falcon.StoredProcedureRunner;
+using Microsoft.EntityFrameworkCore;
+
+namespace Faclon.StoredProcedureRunner.Example
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            IRunner runner = new Runner();
+            var conStr = "Server =.\\SQLSERVER2008R2;Database = test;User ID = sa;Password = 111";
+            var opb = new DbContextOptionsBuilder();
+            opb.UseSqlServer(conStr);
+            var db = new MyDb(opb.Options,runner);
+            var r = db.RunSp1(new Database.sp.Sp1());
+            Console.WriteLine("返回记录数{0},应该为2",r.Count());
+            var fir = r.First();
+            Console.WriteLine("Id为{0},应该为1",fir.Id);
+            Console.WriteLine("Jia{0},应该为3",fir.Jia);
+            Console.WriteLine("Jian{0},应该为-1",fir.Jian);
+            Console.WriteLine("Chen{0},应该为2",fir.Chen);
+            Console.WriteLine("Chu{0},应该为0.5",fir.Chu);
+        }
+    }
+}
