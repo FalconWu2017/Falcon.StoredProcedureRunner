373c6ade7a820a3675a18ced80787e40ef273984
falcon <9504402@qq.com>
2021-01-11 13:11:37 +0800

    IServiceCollection接口增加AddFalconSPRunner方法注册IRunner接口

--- src/Falcon.StoredProcedureRunner/IServiceCollectionExtend.cs
+++ src/Falcon.StoredProcedureRunner/IServiceCollectionExtend.cs
@@ -8,11 +8,20 @@ namespace Falcon.StoredProcedureRunner
     public static class IServiceCollectionExtend
     {
         /// <summary>
-        /// 注册IRunner接口对象，通过该接口可事先 对数据库存储过程模型化调用。
+        /// 注册IRunner接口对象，通过该接口可实现对数据库存储过程模型化调用。
         /// </summary>
         /// <param name="services">注册服务集合</param>
         /// <returns>服务集合</returns>
         public static IServiceCollection UseFalconSPRunner(this IServiceCollection services) {
+            return services.AddFalconSPRunner();
+        }
+
+        /// <summary>
+        /// 注册IRunner接口对象，通过该接口可实现对数据库存储过程模型化调用。
+        /// </summary>
+        /// <param name="services">注册服务集合</param>
+        /// <returns>服务集合</returns>
+        public static IServiceCollection AddFalconSPRunner(this IServiceCollection services) {
             return services.AddSingleton<IRunner,Runner>();
         }
 
